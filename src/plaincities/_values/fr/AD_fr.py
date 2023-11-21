2b8bc3153971cb667c72492b13003cc1d259fe53
brocker brocker <brocker123@protonmail.com>
2023-11-22 01:56:07 +0300

    value files

--- src/plaincities/_values/fr/AD_fr.py
+++ src/plaincities/_values/fr/AD_fr.py
@@ -1,11 +1,16 @@
-ids = [3040051, 3041563]
-names = ["Escaldes-Engordany", "Andorre-la-Vieille"]
-ascii_names = [0, 0]    
-positions = [[42.50729, 1.53414], [42.50779, 1.52109]]
-alternate_names = [["Ehskal'des-Ehndzhordani", "Escaldes", "Escaldes-Engordany", "Les Escaldes", "esukarudesu=engorudani jiao qu", "lai sai si ka er de-en ge er da", "Эскальдес-Энджордани", "エスカルデス＝エンゴルダニ教区", "萊塞斯卡爾德-恩戈爾達", "萊塞斯卡爾德－恩戈爾達"], ["ALV", "Ando-la-Vyey", "Andora", "Andora la Vela", "Andora la Velja", "Andora lja Vehl'ja", "Andoro Malnova", "Andorra", "Andorra Tuan", "Andorra a Vella", "Andorra la Biella", "Andorra la Vella", "Andorra la Vielha", "Andorra-a-Velha", "Andorra-la-Vel'ja", "Andorra-la-Vielye", "Andorre-la-Vieille", "Andò-la-Vyèy", "Andòrra la Vièlha", "an dao er cheng", "andolalabeya", "andwra la fyla", "Ανδόρρα", "Андора ла Веля", "Андора ла Веља", "Андора ля Вэлья", "Андорра-ла-Велья", "אנדורה לה וולה", "أندورا لا فيلا", "አንዶራ ላ ቬላ", "アンドラ・ラ・ヴェリャ", "安道爾城", "안도라라베야"]]
-timezones = [266, 266]
-populations = [15853, 20430]
-altitudes = ["", ""]
-parents = ["AD.08", "AD.07"]
-states = {"AD.06": ["Sant Julià de Lòria", "Sant Julia de Loria", 3039162], "AD.05": ["Ordino", "Ordino", 3039676], "AD.04": ["La Massana", "La Massana", 3040131], "AD.03": ["Encamp", "Encamp", 3040684], "AD.02": ["Canillo", "Canillo", 3041203], "AD.07": ["Andorre-la-Vieille", "Andorra la Vella", 3041566], "AD.08": ["Escaldes-Engordany", "Escaldes-Engordany", 3338529]}
-districts = {}
+geoid_list = [3040051, 3041563]
+name_list = ["Escaldes-Engordany", "Andorre-la-Vieille"]
+ascii_name_list = [0, 0]
+alternate_names_list = [["Ehskal'des-Ehndzhordani", "Escaldes", "Escaldes-Engordany", "Les Escaldes", "esukarudesu=engorudani jiao qu", "lai sai si ka er de-en ge er da", "Эскальдес-Энджордани", "エスカルデス＝エンゴルダニ教区", "萊塞斯卡爾德-恩戈爾達", "萊塞斯卡爾德－恩戈爾達"], ["ALV", "Ando-la-Vyey", "Andora", "Andora la Vela", "Andora la Velja", "Andora lja Vehl'ja", "Andoro Malnova", "Andorra", "Andorra Tuan", "Andorra a Vella", "Andorra la Biella", "Andorra la Vella", "Andorra la Vielha", "Andorra-a-Velha", "Andorra-la-Vel'ja", "Andorra-la-Vielye", "Andorre-la-Vieille", "Andò-la-Vyèy", "Andòrra la Vièlha", "an dao er cheng", "andolalabeya", "andwra la fyla", "Ανδόρρα", "Андора ла Веля", "Андора ла Веља", "Андора ля Вэлья", "Андорра-ла-Велья", "אנדורה לה וולה", "أندورا لا فيلا", "አንዶራ ላ ቬላ", "アンドラ・ラ・ヴェリャ", "安道爾城", "안도라라베야"]]
+latitude_list = [42.50729, 42.50779]
+longitude_list = [1.53414, 1.52109]
+country_code_list = ["AD", "AD"]
+population_list = [15853, 20430]
+dem_list = [1033, 1037]
+timezone_list = [266, 266]
+altitude_list = [-1, -1]
+_feature_class_list = ["P", "P"]
+_feature_code_list = ["PPLA", "PPLC"]
+_parent_code_list = ["AD.08", "AD.07"]
+_states = {"AD.06": ["Sant Julià de Lòria", "Sant Julia de Loria", 3039162], "AD.05": ["Ordino", "Ordino", 3039676], "AD.04": ["La Massana", "La Massana", 3040131], "AD.03": ["Encamp", "Encamp", 3040684], "AD.02": ["Canillo", "Canillo", 3041203], "AD.07": ["Andorre-la-Vieille", "Andorra la Vella", 3041566], "AD.08": ["Escaldes-Engordany", "Escaldes-Engordany", 3338529]}
+_districts = {}
--- src/plaincities/_values/zh/MQ_zh.py
+++ src/plaincities/_values/zh/MQ_zh.py
@@ -1,11 +1,16 @@
-ids = [3569915, 3569926, 3570412, 3570428, 3570429, 3570446, 3570675, 3570785]
-names = ["Saint-Joseph", "Sainte-Marie", "Le Robert", "Le Lamentin", "Le François", "La Trinité", "法兰西堡", "Ducos"]
-ascii_names = [0, 0, 0, 0, "Le Francois", "La Trinite", 0, 0]    
-positions = [[14.67099, -61.03905], [14.78352, -60.99227], [14.67751, -60.94228], [14.61341, -60.99964], [14.61513, -60.90311], [14.73807, -60.96319], [14.60365, -61.07418], [14.57529, -60.97453]]
-alternate_names = [["Saint-Joseph"], ["Bourg de Sainte Marie", "Sainte-Marie", "Sent-Mari", "Сент-Мари"], ["Le Rober", "Le Romper", "Le-Rober", "Robert", "lh rwbr", "lw rwbr", "Λε Ρομπέρ", "Ле Робер", "Ле-Робер", "לה רובר", "لو روبر"], ["Lamentin", "Le Lamanten", "Le Lamenten", "Le Lamentin", "Le-Lamanten", "lw lmwntn", "Λε Λαμεντέν", "Ле Ламантен", "Ле-Ламантен", "לה לאמנטין", "لو لمونتن"], ["Francois", "François", "Le Francois", "Le François", "Le-Fransua", "Ле-Франсуа"], ["La Trinite", "La Trinité", "Trinite", "Trinité"], ["Arx Gallica", "FDF", "Fod Fwans", "For d'o Frans", "For de Frans", "For de Fransas", "For-de-Frans", "For-nte-Frans", "Fordefransa", "Fort Royal", "Fort Royale", "Fort-de-France", "Fort-de-Frans", "Fort-deh-Frans", "Fòd Fwans", "Fôrt-de-France", "Le Fort-de-France", "f xr-dex-fr xngs", "fa lan xi bao", "foru=do=furansu", "fwr dw frans", "fwr-dw-frans", "phorta-de-phransa", "pirancuk kottai", "poleudeupeulangseu", "por-de-pransi", "Φορ-ντε-Φρανς", "Фор де Франс", "Фор дьо Франс", "Фор-де-Франс", "Форт-де-Франс", "Форт-дэ-Франс", "Ֆոր դե Ֆրանս", "פור-דה-פראנס", "فور دو فرانس", "فور-دو-فرانس", "فورٹ ڈی فرانس", "फोर्ट-दे-फ्रान्स", "பிரான்சுக் கோட்டை", "ฟอร์-เดอ-ฟร็องส์", "ფორ-დე-ფრანსი", "フォール＝ド＝フランス", "法兰西堡", "포르드프랑스"], ["Ducos"]]
-timezones = [121, 121, 121, 121, 121, 121, 121, 121]
-populations = [16974, 20380, 23814, 40581, 19682, 15040, 89995, 17394]
-altitudes = ["", "", "", "", "", "", "", ""]
-parents = ["MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972"]
-states = {"MQ.MQ": ["Martinique", "Martinique", 6690603]}
-districts = {"MQ.MQ.972": ["Martinique", "Martinique", 6690604]}
+geoid_list = [3569915, 3569926, 3570412, 3570428, 3570429, 3570446, 3570675, 3570785]
+name_list = ["Saint-Joseph", "Sainte-Marie", "Le Robert", "Le Lamentin", "Le François", "La Trinité", "法兰西堡", "Ducos"]
+ascii_name_list = [0, 0, 0, 0, "Le Francois", "La Trinite", 0, 0]
+alternate_names_list = [["Saint-Joseph"], ["Bourg de Sainte Marie", "Sainte-Marie", "Sent-Mari", "Сент-Мари"], ["Le Rober", "Le Romper", "Le-Rober", "Robert", "lh rwbr", "lw rwbr", "Λε Ρομπέρ", "Ле Робер", "Ле-Робер", "לה רובר", "لو روبر"], ["Lamentin", "Le Lamanten", "Le Lamenten", "Le Lamentin", "Le-Lamanten", "lw lmwntn", "Λε Λαμεντέν", "Ле Ламантен", "Ле-Ламантен", "לה לאמנטין", "لو لمونتن"], ["Francois", "François", "Le Francois", "Le François", "Le-Fransua", "Ле-Франсуа"], ["La Trinite", "La Trinité", "Trinite", "Trinité"], ["Arx Gallica", "FDF", "Fod Fwans", "For d'o Frans", "For de Frans", "For de Fransas", "For-de-Frans", "For-nte-Frans", "Fordefransa", "Fort Royal", "Fort Royale", "Fort-de-France", "Fort-de-Frans", "Fort-deh-Frans", "Fòd Fwans", "Fôrt-de-France", "Le Fort-de-France", "f xr-dex-fr xngs", "fa lan xi bao", "foru=do=furansu", "fwr dw frans", "fwr-dw-frans", "phorta-de-phransa", "pirancuk kottai", "poleudeupeulangseu", "por-de-pransi", "Φορ-ντε-Φρανς", "Фор де Франс", "Фор дьо Франс", "Фор-де-Франс", "Форт-де-Франс", "Форт-дэ-Франс", "Ֆոր դե Ֆրանս", "פור-דה-פראנס", "فور دو فرانس", "فور-دو-فرانس", "فورٹ ڈی فرانس", "फोर्ट-दे-फ्रान्स", "பிரான்சுக் கோட்டை", "ฟอร์-เดอ-ฟร็องส์", "ფორ-დე-ფრანსი", "フォール＝ド＝フランス", "法兰西堡", "포르드프랑스"], ["Ducos"]]
+latitude_list = [14.67099, 14.78352, 14.67751, 14.61341, 14.61513, 14.73807, 14.60365, 14.57529]
+longitude_list = [-61.03905, -60.99227, -60.94228, -60.99964, -60.90311, -60.96319, -61.07418, -60.97453]
+country_code_list = ["MQ", "MQ", "MQ", "MQ", "MQ", "MQ", "MQ", "MQ"]
+population_list = [16974, 20380, 23814, 40581, 19682, 15040, 89995, 17394]
+dem_list = [240, 14, 8, 10, 14, 6, 5, 62]
+timezone_list = [121, 121, 121, 121, 121, 121, 121, 121]
+altitude_list = [-1, -1, -1, -1, -1, -1, -1, -1]
+_feature_class_list = ["P", "P", "P", "P", "P", "P", "P", "P"]
+_feature_code_list = ["PPL", "PPL", "PPL", "PPL", "PPL", "PPL", "PPLC", "PPL"]
+_parent_code_list = ["MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972", "MQ.MQ.972"]
+_states = {"MQ.MQ": ["Martinique", "Martinique", 6690603]}
+_districts = {"MQ.MQ.972": ["Martinique", "Martinique", 6690604]}
